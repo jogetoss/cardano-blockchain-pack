2e5b03f83869e97c4bde4e7cb6d10be28ae386ae
justin <justin@joget.org>
2021-10-18 15:07:42 +0800

    Updated cardano lib for Address Utxo API change fix

--- src/main/java/org/joget/marketplace/CardanoSendTransactionTool.java
+++ src/main/java/org/joget/marketplace/CardanoSendTransactionTool.java
@@ -14,13 +14,9 @@ import com.bloxbean.cardano.client.transaction.model.PaymentTransaction;
 import com.bloxbean.cardano.client.account.Account;
 import com.bloxbean.cardano.client.backend.api.BackendService;
 import com.bloxbean.cardano.client.backend.api.BlockService;
-import com.bloxbean.cardano.client.backend.api.EpochService;
 import com.bloxbean.cardano.client.backend.api.TransactionService;
-import com.bloxbean.cardano.client.backend.api.UtxoService;
 import com.bloxbean.cardano.client.backend.api.helper.FeeCalculationService;
 import com.bloxbean.cardano.client.backend.api.helper.TransactionHelperService;
-import com.bloxbean.cardano.client.backend.api.helper.UtxoSelectionStrategy;
-import com.bloxbean.cardano.client.backend.api.helper.impl.FeeCalculationServiceImpl;
 import com.bloxbean.cardano.client.backend.api.helper.model.TransactionResult;
 import com.bloxbean.cardano.client.backend.exception.ApiException;
 import com.bloxbean.cardano.client.backend.model.Result;
@@ -34,7 +30,6 @@ import com.bloxbean.cardano.client.metadata.Metadata;
 import com.bloxbean.cardano.client.metadata.cbor.CBORMetadata;
 import com.bloxbean.cardano.client.metadata.cbor.CBORMetadataMap;
 import com.bloxbean.cardano.client.transaction.model.TransactionDetailsParams;
-import com.bloxbean.cardano.client.util.JsonUtil;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -142,11 +137,6 @@ public class CardanoSendTransactionTool extends DefaultApplicationPlugin {
             
             BlockService blockService = backendService.getBlockService();
             TransactionHelperService transactionHelperService = backendService.getTransactionHelperService();
-
-            /* Temporary fix due to Blockfrost API changes */
-            UtxoService utxoService = backendService.getUtxoService();
-            UtxoSelectionStrategy tempUtxoSelectionStrategy = new TempWorkaroundUtxoSelectionStrategyImpl(utxoService);
-            transactionHelperService.getUtxoTransactionBuilder().setUtxoSelectionStrategy(tempUtxoSelectionStrategy);
             
             long ttl = blockService.getLastestBlock().getValue().getSlot() + 1000;
             TransactionDetailsParams detailsParams = TransactionDetailsParams.builder().ttl(ttl).build();
@@ -231,14 +221,7 @@ public class CardanoSendTransactionTool extends DefaultApplicationPlugin {
     }
     
     private BigInteger calculateFeeFromTransactionSize(BackendService backendService, TransactionDetailsParams detailsParams, PaymentTransaction paymentTransaction, Metadata metadata) throws ApiException, CborSerializationException, AddressExcepion {
-        /* Temporary fix due to Blockfrost API changes */
-        UtxoService utxoService = backendService.getUtxoService();
-        UtxoSelectionStrategy tempUtxoSelectionStrategy = new TempWorkaroundUtxoSelectionStrategyImpl(utxoService);
-        TransactionHelperService transactionHelperService = backendService.getTransactionHelperService();
-        EpochService epochService = backendService.getEpochService();
-        transactionHelperService.getUtxoTransactionBuilder().setUtxoSelectionStrategy(tempUtxoSelectionStrategy);
-        
-        FeeCalculationService feeCalculationService = new FeeCalculationServiceImpl(transactionHelperService, epochService);
+        FeeCalculationService feeCalculationService = backendService.getFeeCalculationService();
         
         //Calculate fee from transaction size in bytes
         return feeCalculationService.calculateFee(paymentTransaction, detailsParams, metadata);
