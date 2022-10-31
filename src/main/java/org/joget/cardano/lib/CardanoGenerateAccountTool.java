8f0c39f3902e97d1c6f49dff6dd2bffef83bc312
justin <justin@joget.org>
2022-10-31 17:41:27 +0800

    Cardano Generate Account Tool - Store Account Data - Make "Account Base Address" property editable

--- src/main/java/org/joget/cardano/lib/CardanoGenerateAccountTool.java
+++ src/main/java/org/joget/cardano/lib/CardanoGenerateAccountTool.java
@@ -11,6 +11,7 @@ import org.joget.apps.app.service.AppService;
 import org.joget.apps.app.service.AppUtil;
 import org.joget.apps.form.model.FormRow;
 import org.joget.apps.form.model.FormRowSet;
+import org.joget.apps.form.service.FormUtil;
 import org.joget.cardano.model.CardanoProcessToolAbstract;
 import org.joget.commons.util.LogUtil;
 import org.joget.workflow.model.WorkflowAssignment;
@@ -91,7 +92,7 @@ public class CardanoGenerateAccountTool extends CardanoProcessToolAbstract {
     protected void storeToForm(boolean isTest, final Account account, WorkflowAssignment wfAssignment) {
         String formDefId = getPropertyString("formDefId");
         
-        String accountBaseAddressField = getPropertyString("accountBaseAddressField");
+        String accountBaseAddressField = getPropertyString("accountBaseAddress");
         String accountMnemonicField = getPropertyString("accountMnemonicField");
         String accountOwnerField = getPropertyString("accountOwnerField");
         String accountOwnerValue = WorkflowUtil.processVariable(getPropertyString("accountOwnerValue"), "", wfAssignment);
@@ -100,9 +101,11 @@ public class CardanoGenerateAccountTool extends CardanoProcessToolAbstract {
 
         FormRow row = new FormRow();
 
-        //Account base address set as Record ID
-        //row.setId(account.baseAddress());
-        row = addRow(row, accountBaseAddressField, account.baseAddress());
+        if ((FormUtil.PROPERTY_ID).equals(accountBaseAddressField)) {
+            row.setId(account.baseAddress());
+        } else {
+            row = addRow(row, accountBaseAddressField, account.baseAddress());
+        }
         
         //Mnemonic phrase MUST be secured at all times.
         row = addRow(row, accountMnemonicField, PluginUtil.encrypt(account.mnemonic()));
