9fe0a73413626133eb957e1313cb15d1cd4e427e
A Ady <adi.arshad200@gmail.com>
2023-11-29 03:40:34 +0800

    changes made

--- src/main/java/org/joget/cardano/lib/Activator.java
+++ src/main/java/org/joget/cardano/lib/Activator.java
@@ -22,39 +22,45 @@ public class Activator implements BundleActivator {
     protected Collection<ServiceRegistration> registrationList;
 
     @Override
-    public void start(BundleContext context) {
-        registrationList = new ArrayList<ServiceRegistration>();
+    public void start(BundleContext context) {registrationList = new ArrayList<ServiceRegistration>();
 
-        //Process Tool plugins
-        registrationList.add(context.registerService(CardanoGenerateAccountTool.class.getName(), new CardanoGenerateAccountTool(), null));
-        registrationList.add(context.registerService(CardanoSendTransactionTool.class.getName(), new CardanoSendTransactionTool(), null));
+        // Process Tool plugins
+        registrationList.add(context.registerService(CardanoGenerateAccountTool.class.getName(),new CardanoGenerateAccountTool(), null));
+        registrationList.add(context.registerService(CardanoSendTransactionTool.class.getName(),new CardanoSendTransactionTool(), null));
         registrationList.add(context.registerService(CardanoMintTokenTool.class.getName(), new CardanoMintTokenTool(), null));
         registrationList.add(context.registerService(CardanoBurnTokenTool.class.getName(), new CardanoBurnTokenTool(), null));
-        
-        //Form Binder plugins
-        registrationList.add(context.registerService(CardanoAccountLoadBinder.class.getName(), new CardanoAccountLoadBinder(), null));
-        registrationList.add(context.registerService(CardanoMetadataLoadBinder.class.getName(), new CardanoMetadataLoadBinder(), null));
-        
-        //Form Element plugins
-        registrationList.add(context.registerService(CardanoExplorerLinkFormElement.class.getName(), new CardanoExplorerLinkFormElement(), null));
-        
-        //Default Properties plugins
-        registrationList.add(context.registerService(CardanoDefaultBackendPlugin.class.getName(), new CardanoDefaultBackendPlugin(), null));
-        
-        //Web Service plugins
+
+        // Form Binder plugins
+        registrationList.add(context.registerService(CardanoAccountLoadBinder.class.getName(),new CardanoAccountLoadBinder(), null));
+        registrationList.add(context.registerService(CardanoMetadataLoadBinder.class.getName(),new CardanoMetadataLoadBinder(), null));
+        registrationList.add(context.registerService(CardanoTokenLoadBinder.class.getName(),new CardanoMetadataLoadBinder(), null));
+
+
+        // Form Element plugins
+        registrationList.add(context.registerService(CardanoExplorerLinkFormElement.class.getName(),new CardanoExplorerLinkFormElement(), null));
+
+        // Default Properties plugins
+        registrationList.add(context.registerService(CardanoDefaultBackendPlugin.class.getName(),
+                new CardanoDefaultBackendPlugin(), null));
+
+        // Web Service plugins
         registrationList.add(context.registerService(HelperWebService.class.getName(), new HelperWebService(), null));
-        
-        //Process Form Modifier plugins
-        registrationList.add(context.registerService(CardanoTransactionExecutor.class.getName(), new CardanoTransactionExecutor(), null));
-        registrationList.add(context.registerService(CustomCompleteButton.class.getName(), new CustomCompleteButton(), null));
-        
-        //Transaction Action plugins (Custom)
-        registrationList.add(context.registerService(TokenTransferAction.class.getName(), new TokenTransferAction(), null));
+
+        // Process Form Modifier plugins
+        registrationList.add(context.registerService(CardanoTransactionExecutor.class.getName(),
+                new CardanoTransactionExecutor(), null));
+        registrationList
+                .add(context.registerService(CustomCompleteButton.class.getName(), new CustomCompleteButton(), null));
+
+        // Transaction Action plugins (Custom)
+        registrationList
+                .add(context.registerService(TokenTransferAction.class.getName(), new TokenTransferAction(), null));
         registrationList.add(context.registerService(TokenMintAction.class.getName(), new TokenMintAction(), null));
         registrationList.add(context.registerService(TokenBurnAction.class.getName(), new TokenBurnAction(), null));
-        
-        //Custom Interfaces
-        PluginManager.registerCustomPluginInterface(new CustomPluginInterface(CardanoTransactionPlugin.class, "cardano.plugin.transactionAction", MESSAGE_PATH));
+
+        // Custom Interfaces
+        PluginManager.registerCustomPluginInterface(new CustomPluginInterface(CardanoTransactionPlugin.class,
+                "cardano.plugin.transactionAction", MESSAGE_PATH));
     }
 
     @Override
---
+++ src/main/java/org/joget/cardano/lib/CardanoTokenLoadBinder.java
@@ -0,0 +1,183 @@
+package org.joget.cardano.lib;
+
+import com.bloxbean.cardano.client.api.exception.ApiException;
+import org.joget.cardano.util.PluginUtil;
+import com.bloxbean.cardano.client.backend.model.Asset;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.bloxbean.cardano.client.api.model.Result;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.form.model.Element;
+import org.joget.apps.form.model.FormData;
+import org.joget.apps.form.model.FormLoadElementBinder;
+import org.joget.apps.form.model.FormRow;
+import org.joget.apps.form.model.FormRowSet;
+import org.joget.commons.util.LogUtil;
+import org.joget.workflow.util.WorkflowUtil;
+import java.util.HashMap;
+import java.util.Map;
+import org.joget.cardano.model.CardanoFormBinder;
+
+public class CardanoTokenLoadBinder extends CardanoFormBinder implements FormLoadElementBinder {
+    @Override
+    public String getName() {
+        return "Cardano Token Load Binder";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Load token data from the Cardano blockchain into a form.";
+    }
+
+    @Override
+    public String getPropertyOptions() {
+        String backendConfigs = PluginUtil.readGenericBackendConfigs(getClassName());
+        return AppUtil.readPluginResource(getClassName(), "/properties/CardanoTokenLoadBinder.json",
+                new String[] { backendConfigs }, true, PluginUtil.MESSAGE_PATH);
+    }
+
+    @Override
+    public boolean isInputDataValid() {
+        final String assetId = WorkflowUtil.processVariable(getPropertyString("assetId"), "", null);
+
+        // Prevent error thrown from empty value and invalid hash variable
+        return !assetId.isEmpty() && !assetId.startsWith("#");
+    }
+
+    @Override
+    public FormRowSet loadData(Element element, String primaryKey, FormData formData)
+            throws RuntimeException {
+
+        try {
+            final String assetId = WorkflowUtil.processVariable(getPropertyString("assetId"), "", null);
+            final String assetName = getPropertyString("assetName");
+            final String policyId = getPropertyString("policyId");
+            final String fingerprint = getPropertyString("fingerprint");
+            final String quantity = getPropertyString("quantity");
+            final String initialMintTxHash = getPropertyString("initialMintTxHash");
+            final String mintOrBurnCount = getPropertyString("mintOrBurnCount");
+            final String onchainMetadata = getPropertyString("onchainMetadata");
+
+            final Result<Asset> assetInfoResult = assetService.getAsset(assetId);
+
+            if (!assetInfoResult.isSuccessful()) {
+                LogUtil.warn(getClassName(),
+                        "Unable to retrieve token info. Response returned --> " + assetInfoResult.getResponse());
+                return null;
+            }
+
+            final Asset assetInfo = assetInfoResult.getValue();
+
+            FormRow row = new FormRow();
+
+            row = addRow(row, assetName, getAssetName(assetInfo));
+            row = addRow(row, policyId, getPolicyId(assetInfo));
+            row = addRow(row, fingerprint, getFingerprint(assetInfo));
+            row = addRow(row, quantity, getQuantity(assetInfo));
+            row = addRow(row, initialMintTxHash, getInitialMintTxHash(assetInfo));
+            row = addRow(row, mintOrBurnCount, getMintOrBurnCount(assetInfo));
+            row = addRow(row, onchainMetadata, getOnchainMetadata(assetInfo));
+
+            FormRowSet rows = new FormRowSet();
+            rows.add(row);
+
+            return rows;
+        } catch (ApiException e) {
+            throw new RuntimeException(e.getClass().getName() + " : " + e.getMessage());
+        }
+    }
+
+    private String getOnchainMetadata(Asset assetInfo) {
+        final JsonNode onChainData = assetInfo.getOnchainMetadata();
+        if (onChainData != null) {
+            LogUtil.info(getClass().getName(), "onchainMetadata JSON: " + onChainData.toString());
+
+            // Assuming you have an ObjectMapper instance
+            ObjectMapper objectMapper = new ObjectMapper();
+            // Convert JsonNode to a Map
+            Map<String, Object> onChainDataMap = objectMapper.convertValue(onChainData, Map.class);
+            onChainDataMap = onChainDataMap != null ? onChainDataMap : new HashMap<>();
+
+            // Convert the Map to a JSON string
+            try {
+                return objectMapper.writeValueAsString(onChainDataMap);
+            } catch (Exception e) {
+                LogUtil.warn(getClass().getName(),
+                        "Error converting on-chain metadata to JSON string: " + e.getMessage());
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    // private String getMetadata(Asset assetInfo) {
+    // final JsonNode metadata = assetInfo.getMetadata();
+    // if (metadata != null) {
+    // LogUtil.info(getClass().getName(), "metadata JSON: " + metadata.toString());
+
+    // // Assuming you have an ObjectMapper instance
+    // ObjectMapper objectMapper = new ObjectMapper();
+    // // Convert JsonNode to a Map
+    // Map<String, Object> metadataMap = objectMapper.convertValue(metadata,
+    // Map.class);
+    // metadataMap = metadataMap != null ? metadataMap : new HashMap<>();
+
+    // // Convert the Map to a JSON string
+    // try {
+    // return objectMapper.writeValueAsString(metadataMap);
+    // } catch (Exception e) {
+    // LogUtil.warn(getClass().getName(), "Error converting metadata to JSON string:
+    // " + e.getMessage());
+    // return null;
+    // }
+    // } else {
+    // return null;
+    // }
+    // }
+
+    private String getMintOrBurnCount(Asset assetInfo) {
+        String mintOrBurnCount = String.valueOf(assetInfo.getMintOrBurnCount());
+
+        return mintOrBurnCount;
+    }
+
+    private String getInitialMintTxHash(Asset assetInfo) {
+        String initialMintTxHash = assetInfo.getInitialMintTxHash();
+
+        return initialMintTxHash;
+    }
+
+    private String getQuantity(Asset assetInfo) {
+        String quantity = assetInfo.getQuantity();
+
+        return quantity;
+    }
+
+    private String getFingerprint(Asset assetInfo) {
+        String fingerprint = assetInfo.getFingerprint();
+
+        return fingerprint;
+    }
+
+    private String getAssetName(Asset assetInfo) {
+        String name = assetInfo.getAssetName();
+
+        return name;
+    }
+
+    private String getPolicyId(Asset assetInfo) {
+        String policy = assetInfo.getPolicyId();
+
+        return policy;
+    }
+
+    private FormRow addRow(FormRow row, String field, String value) {
+        if (row != null && !field.isEmpty()) {
+            row.put(field, value);
+        }
+
+        return row;
+    }
+
+}
